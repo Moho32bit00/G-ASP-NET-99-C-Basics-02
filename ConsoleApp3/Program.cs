0bbcb15d4cd420c71737a5453f42bac3011f744f
Moho32bit00 <mohamed.man2007@gmail.com>
2026-08-28 19:10:48 +0300

    feat: implement Book class boxing exercise
    Create a Book class with a Title (string) and Pages (int).
    Create a Book object and store it in a variable of type object. Print it.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace ConsoleApp3
 {
+
     internal class Program
     {
+        internal class Book
+        {
+            public string Title { get; set; }
+            public int Pages { get; set; }
+
+
+
+            #region override
+            //public override bool Equals(object? obj)
+            //{
+            //    if (obj is not Book) { return false; }
+
+            //    Book b1 = obj as Book;
+
+            //    return b1.Title == Title && b1.Pages == Pages;
+
+            //}
+            //public override int GetHashCode()
+            //{
+            //    return HashCode.Combine(Title, Pages);
+            //}
+            //public override string ToString()
+            //{
+            //    return $"Tiltle = {Title} , pAges = {Pages}";
+            //}
+            #endregion
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region q1
+            //object b2 = new Book() { Title = "berserk" , Pages =4000 }; 
+
+            //Console.WriteLine(b2.ToString());
+            //// to display the content uncomment oveeride reigon on Book class .
+            #endregion
+
+
+
+
         }
     }
+
 }
